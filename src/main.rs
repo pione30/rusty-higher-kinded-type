31d5bfabadda7da4d43ec11c99eccb31cdcdba8a
pione30 <g.pionelish30@gmail.com>
2022-05-15 22:46:57 +0900

    Define trait Functor<U>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ trait HKT<U> {
     type MU; // Type with C mapped to U
 }
 
+trait Functor<U>: HKT<U> {
+    fn map<F>(self, f: F) -> Self::MU
+    where
+        F: FnOnce(Self::C) -> U;
+}
+
 impl<T, U> HKT<U> for Option<T> {
     type C = T;
     type MU = Option<U>;
